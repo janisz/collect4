9cda49d146f485a750273f54aa1fe7008c90233d
Jakub Dutkowski <kdutkowski@gmail.com>
2015-04-25 12:29:00 +0200

    refactored names to camel case

--- BoardGenerator/BoardGenerator.go
+++ BoardGenerator/BoardGenerator.go
@@ -5,29 +5,29 @@ import (
 	"strings"
 )
 
-func generate_all_possible_boards() {
+func generateAllPossibleBoards() {
 	board := [16]string{}
 	for i := range board {
 		board[i] = "0"
 	}
-	generate_board(board, 0)
+	generateBoard(board, 0)
 }
 
-func generate_board(vector [16]string, depth int) {
+func generateBoard(vector [16]string, depth int) {
 	if depth == 2 {
 		fmt.Printf("%s\n", strings.Join(vector[:], ","))
 		return
 	}
 	next_depth := depth + 1
 	vector[depth] = "-1"
-	generate_board(vector, next_depth)
+	generateBoard(vector, next_depth)
 	vector[depth] = "1"
-	generate_board(vector, next_depth)
+	generateBoard(vector, next_depth)
 	vector[depth] = "0"
-	generate_board(vector, next_depth)
+	generateBoard(vector, next_depth)
 
 }
 
 func main() {
-	generate_all_possible_boards()
+	generateAllPossibleBoards()
 }
